dedf44899d3318736c743a0c3b106a7523b7aff4
Felipe Marinho <felipmarinho@gmail.com>
2019-07-06 21:47:28 -0300

    Adding a listing of schedules of a provider

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import UserController from './app/controllers/UserController';
 import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
 import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
@@ -24,6 +25,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
