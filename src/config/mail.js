5da7b9a48230026d220c5a50c57ac7cb8fd187d3
Felipe Marinho <felipmarinho@gmail.com>
2019-07-07 15:40:11 -0300

    Adding and setting  mailtrap and functionality of send mail when appointment be canceled

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4ce352f64d624c',
+    pass: 'e34484e09cb6ce',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
